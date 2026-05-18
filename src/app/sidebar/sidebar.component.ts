3cdb9385d0a3e0a76735dc98f8b92c336727a8d6
Sebastian Steinau <32056906+ssteinau@users.noreply.github.com>
2026-05-18 16:54:15 +0200

    submit file

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -18,6 +18,8 @@ export class SidebarComponent {
   @Output() sidebarToggle = new EventEmitter<void>();
 
   routeList:string[][] = [
+    [ 'DashboardComponent'
+    ],
   ]
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
